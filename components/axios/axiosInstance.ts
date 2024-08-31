9283ce0f4ce5441908167288920b6cd342a153d5
davidktlee <rudxor567@naver.com>
2024-08-31 23:59:20 +0900

    feat: android api 요청을 위한 로직 추가

--- components/axios/axiosInstance.ts
+++ components/axios/axiosInstance.ts
@@ -1,7 +1,16 @@
 import axios from 'axios'
+import { Platform } from 'react-native'
+import Constants from 'expo-constants'
+
+// android의 로컬 서버로의 api 요청을 위한 ip 주소 get 로직
+const HOST = Constants.expoConfig?.hostUri?.split(':').shift()
 
 const BASE_URL =
-  process.env.ENVIRONMENT === 'production' ? process.env.API_URL : 'http://localhost:5002'
+  process.env.ENVIRONMENT === 'production'
+    ? process.env.API_URL
+    : Platform.OS === 'android'
+    ? `http://${HOST}:5002`
+    : 'http://localhost:5002'
 
 export const api = axios.create({
   baseURL: BASE_URL,
